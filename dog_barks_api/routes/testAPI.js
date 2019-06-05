cc3d6e2d7376e344eec68b2be85a4ae484901859
hugo escandon <hescandon6@gmail.com>
2019-06-04 20:12:24 -0400

    first commit

---
+++ dog_barks_api/routes/testAPI.js
@@ -0,0 +1,3 @@
+var express = require('express');var router = express.Router();
+router.get('/', function(req, res, next) {    res.send('API is working properly hugo the man');});
+module.exports = router;
