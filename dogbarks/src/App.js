244b82de71feef50b36a39e2bb07ffb53c1dc078
hugo escandon <hescandon6@gmail.com>
2019-06-13 18:44:19 -0400

    Google Maps Api integration

--- dogbarks/src/App.js
+++ dogbarks/src/App.js
@@ -2,38 +2,43 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import MapContainer from "./containers/mapContainer"
+
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { apiResponse: "" };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
 
-    callAPI() {
-        fetch("http://localhost:9000/users")
-            .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }))
-            .catch(err => err);
-    }
+  callAPI() {
+    fetch("http://localhost:9000/users")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }))
+      .catch(err => err);
+  }
 
-    componentDidMount() {
-        this.callAPI();
-    }
+  componentDidMount() {
+    this.callAPI();
+  }
+  
 
-    render() {
-      console.log(this.state.apiResponse);
+  render() {
+    console.log(this.state.apiResponse);
 
-        return (
-
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                    <p className="App-intro">{this.state.apiResponse}</p>
-                </header>
-
-            </div>
-        );
-    }
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+          <p className="App-intro">{this.state.apiResponse}</p>
+        </header>
+        <MapContainer/>
+      </div>
+    );
+  }
 }
+// <div>
+//   <h1>{this.state.selectedPlace.name}</h1>
+// </div>
 
-export default App;
+export default(App);
---
+++ dogbarks/src/containers/mapContainer.js
@@ -0,0 +1,58 @@
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+import React, { Component } from "react";
+
+class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onMapClicked = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+  fetchPlaces(mapProps, map) {
+  const {google} = mapProps;
+  const service = new google.maps.places.PlacesService(map);
+  // ...
+}
+
+  render() {
+    return (
+      <Map
+        style={{ width: "100%", height: "75%", position: "relative" }}
+        zoom={14}
+        google={this.props.google}
+        onClick={this.onMapClicked}
+        onReady={this.fetchPlaces}
+      >
+        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+        <InfoWindow
+          marker={this.state.activeMarker}
+
+          visible={this.state.showingInfoWindow}
+        >
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyC9K0HchhMZBCaitQcx_MnT7eDJav6s4tQ"
+})(MapContainer);
