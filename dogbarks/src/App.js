8a32af3e9492a3c37a61e14ed88fa267fd81e5d0
hugo escandon <hescandon6@gmail.com>
2019-11-04 11:41:09 -0500

    New UI 1.0

--- dogbarks/src/App.js
+++ dogbarks/src/App.js
@@ -1,46 +1,7 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react'
+// import './index.css'
 
 
+import routes from './routes'
 
-import MapContainer from "./containers/mapContainer"
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callAPI() {
-    fetch("http://localhost:9000/users")
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }))
-      .catch(err => err);
-  }
-
-  componentDidMount() {
-    this.callAPI();
-  }
-
-
-  render() {
-    console.log(this.state.apiResponse);
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <p className="App-intro">{this.state.apiResponse}</p>
-        </header>
-        <MapContainer/>
-      </div>
-    );
-  }
-}
-// <div>
-//   <h1>{this.state.selectedPlace.name}</h1>
-// </div>
-
-export default(App);
+export default () => <>{ routes }</>
--- dogbarks/src/routes.js
+++ dogbarks/src/routes.js
@@ -1,5 +1,17 @@
-import Login from "./containers/Login";
+import React from "react";
+import Login from "./containers/login";
+import Users from "./containers/users"
+import MapContainer from "./containers/mapContainer"
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 
+export default (
+
+  <BrowserRouter>
 <Route path="/login" exact component={Login} />
+<Route path="/users" exact component={Users}/>
+<Route path="/mapContainer" exact component={MapContainer}/>
+
+
+  </BrowserRouter>
+)
