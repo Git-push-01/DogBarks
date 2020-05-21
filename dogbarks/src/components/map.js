ceae036e5d3afb5f64ab089f77f3c1291b062e5a
hugo escandon <hescandon6@gmail.com>
2020-05-21 13:49:17 -0400

    user location and search working 1.0

--- dogbarks/src/components/map.js
+++ dogbarks/src/components/map.js
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
-import ReactMapGL, { GeolocateControl } from "react-map-gl";
+import ReactMapGL, { GeolocateControl, NavigationControl } from "react-map-gl";
 import config from "../config";
 import "mapbox-gl/dist/mapbox-gl.css";
-import MapGL, { NavigationControl } from "react-map-gl";
 import Geocoder from "react-map-gl-geocoder";
-import DeckGL, { GeoJsonLayer } from "deck.gl";
 import { withRouter } from "react-router-dom";
 import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
-
 
 const TOKEN = config.REACT_APP_TOKEN;
 
@@ -38,34 +35,6 @@ const Map = () => {
     minPitch: 0,
     maxPitch: 85,
   });
-
-  // const handleViewportChange = (viewport) => {
-  //   this.setState({
-  //     viewport: { ...this.state.viewport, ...viewport },
-  //   });
-  // };
-  // if you are happy with Geocoder default settings, you can just use handleViewportChange directly
-  // const handleGeocoderViewportChange = viewport => {
-  //   const geocoderDefaultOverrides = { transitionDuration: 1000 };
-  //
-  //   return this.handleViewportChange({
-  //     ...viewport,
-  //     ...geocoderDefaultOverrides,
-  //   });
-  // };
-
-  // const handleOnResult = (event) => {
-  //   this.setState({
-  //     searchResultLayer: new GeoJsonLayer({
-  //       id: "search-result",
-  //       data: event.result.geometry,
-  //       getFillColor: [255, 0, 0, 128],
-  //       getRadius: 1000,
-  //       pointRadiusMinPixels: 10,
-  //       pointRadiusMaxPixels: 10,
-  //     }),
-  //   });
-  // };
 
   const mapRef = React.useRef();
 
@@ -113,7 +82,6 @@ const Map = () => {
           showUserLocation={true}
           position="top-left"
         />
-
       </ReactMapGL>
     </div>
   );
--- dogbarks/src/containers/mapContainer.js
+++ dogbarks/src/containers/mapContainer.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-import config from "../config";
 import Map from "../components/map";
 
-const TOKEN = config.REACT_APP_TOKEN;
+
 
 class MapContainer extends Component {
   // componentWillMount() {
